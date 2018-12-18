554fd6f34a9f9024febc7e232139936ee91643c5
assetozen <assetozen@yahoo.com>
2018-12-18 16:53:59 +0100

    First Commit

---
+++ LearningMaven/src/main/java/com/assegd/LearningMaven/App.java
@@ -0,0 +1,13 @@
+package com.assegd.LearningMaven;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
